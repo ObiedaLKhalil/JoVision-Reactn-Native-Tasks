a9107cc768d30096957126321d359dca6630e83f
ObiedaLKhalil <olkhalil2024@gmail.com>
2024-07-04 10:29:25 +0300

    edit app.tsx

--- App.tsx
+++ App.tsx
@@ -1,45 +1,10 @@
-import React, { useState,useEffect } from 'react';
-import {ActivityIndicator, StyleSheet,Text, View} from 'react-native';
+import Task18 from './Tasks/Task18';
 
 const App = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    
-  useEffect(() => {
- setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
+  return (
+ 
+    <Task18  />  
+  );
+};
 
-    
-    return () =>setIsLoading(true);
-  }, []);
-    return (
-      
-      <View style={[styles.container, styles.horizontal]}>
-        {isLoading ?<ActivityIndicator animating={isLoading}/>
-        : 
-          <Text>my  name is Obieda Khalil</Text>
-        }
-      </View>
-    );
-  };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 30,
-    marginTop: 20,
-  },
-  horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10,
-  },
-});
 export default App;
-
-
-
