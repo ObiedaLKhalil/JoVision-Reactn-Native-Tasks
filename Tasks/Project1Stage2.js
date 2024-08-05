614327f81f93f1c0a790d59f6c2ac08868e7ddba
ObiedaLKhalil <olkhalil2024@gmail.com>
2024-08-05 11:44:11 +0300

    end Project1Stage2Screen3

--- Tasks/Project1Stage2.js
+++ Tasks/Project1Stage2.js
@@ -3,6 +3,7 @@ import {SafeAreaView,StyleSheet,TextInput,View,Text, Button, Image,Modal,ImageBa
 import { launchCamera } from 'react-native-image-picker';
 import RNFS from 'react-native-fs';
 import { NavigationContainer } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Tab =createBottomTabNavigator();
 import Geolocation from 'react-native-geolocation-service';
@@ -21,14 +22,17 @@ const App = () => {
     const [loadedPhotos, setLoadedPhotos] = useState([]);
     const [loadedVideos, setLoadedVideos] = useState([]);
     const [refreshing, setRefreshing] = React.useState(false);
-    const [isPlaying, setIsPlaying] = useState(true);
+    //const [isPlaying, setIsPlaying] = useState(true);
     const [loadPhotosVideos,setloadPhotosVideos] = useState([]);
     const [newName,setNewName]=useState("");
     const [visible,setVisible]=useState(false);
     const [submit,setsubmit]=useState(false);
+    const[passItem,setpassItem]=useState(null);
+    const[ispassItem,setispassItem]=useState(false);
+    const[isnagivate,setisnagivate]=useState(false);
    // const [isBuffering, setIsBuffering] = useState(false);
     const flatListRef1=useRef(null);
-    const flatListRef2=useRef(null);
+   // const flatListRef2=useRef(null);
     const X =5;
     const Y =3;
     const images = {
@@ -211,7 +215,6 @@ const requestStoragePermissionToRename = async () => {
     setCameraMode(prevType => (prevType === 'camera' ? 'video' : 'camera'));
   };
   const TakePhotoOrRecordVideo = () => {
-    
    const cameraPermission =  requestCameraPermissions();
       if (!cameraPermission) {
         console.log('camera and storage permission denied');
@@ -279,6 +282,28 @@ else{
         console.log('File deleted');
       
         setPhoto(null);
+        loadPhotosFunction();
+      } else {
+        console.log('File does not exist');
+      }
+    } catch (error) {
+      console.error('Error deleting file:', error);
+    }
+  };
+  const deleteFun =  (item) => {
+    try {
+      const storagePermission =  requestStoragePermissionToDelete();
+      if (!storagePermission) {
+        console.log('Storage permission denied');
+        return;
+      }
+      const filePath = item.path;
+      console.log('Deleting file at path:', filePath);
+      const fileExists =  RNFS.exists(filePath);
+      if (fileExists) {
+         RNFS.unlink(filePath);
+        console.log('File deleted');
+        loadPhotosFunction();
       } else {
         console.log('File does not exist');
       }
@@ -320,7 +345,7 @@ else{
         }
         
     try {
-      const files = await RNFS.readDir(RNFS.ExternalStorageDirectoryPath + '/Pictures');
+      const files = await RNFS.readDir(RNFS.DocumentDirectoryPath + '/Pictures');
       const imageFiles = files.filter(file => file.isFile() && /\.(jpe?g|png)$/i.test(file.name));
       setLoadedPhotos(imageFiles);
     } catch (error) {
@@ -333,7 +358,7 @@ else{
             }
             
         try {
-          const files = await RNFS.readDir(RNFS.ExternalStorageDirectoryPath + '/Movies');
+          const files = await RNFS.readDir(RNFS.DocumentDirectoryPath + '/Movies');
           const videoFiles = files.filter(file => file.isFile() && /\.mp4$/i.test(file.name));
           setLoadedVideos(videoFiles);
         } catch (error) {
@@ -361,6 +386,8 @@ else{
         'Rename,Delete,FullScreen',
         [
           { text: 'Rename', onPress: () =>RenameFun(item) },
+          { text: 'Delete', onPress: () =>deleteFun(item) },
+          { text: 'FullScreen', onPress: () =>FullScreenFun(item) },
         ],
         { cancelable: false }
       );
@@ -394,6 +421,13 @@ const submitFunction =()=>{
 setsubmit(true);
 
 };
+
+const FullScreenFun = (item)=>{
+  setisnagivate(true);
+  setisnagivate(false);
+  setpassItem(item); 
+ setispassItem(true);
+};
       const onRefresh1 = React.useCallback(() => {
             setRefreshing(true);
             loadPhotosVideosFun().then(() => setTimeout(() => {
@@ -405,158 +439,169 @@ setsubmit(true);
   const ChangeText = (newT) => {
     setNewName(newT);
   };
-
-   
-
-    const Screen1 = ({ navigation }) => (
-        <SafeAreaView style={styles.container}>
   
-        <Button title="Switch camera" onPress={toggleCameraType} color="orange" />
-        <Button title="Switch mode" onPress={toggleCameraMode} color="red" />
-        <Button title="Take photo/ Record video" onPress={TakePhotoOrRecordVideo} color="green" />
-        {photo && <Image source={{ uri: photo }} style={{ width: 200, height: 200 }} />}
-    </SafeAreaView>
-      );
-      const Screen2 = ({ navigation }) => {
-        useEffect(()  => {
-          const getLocation=()=>{
-          const locationPermission =  requestLocationPermission();
-          if (!locationPermission) {
-            console.log('location permission denied');
-            return;
-          }
-          Geolocation.getCurrentPosition(
-            (position) => {
-              setLocation(position);
-            },
-            (error) => {
-              console.error(error);
-            },
-            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-          );};
-          getLocation();
-          const locationInterval = setInterval(getLocation, 10000);
-          return () => {
-            clearInterval(locationInterval);
-           
-          };
-        }, [location]);
-          useEffect(()  => {
 
-          setUpdateIntervalForType(SensorTypes.accelerometer, 500);
-          const accelerometerSubscription = accelerometer
-          .pipe(
-            map(({ x, y, z }) => ({ x, y, z })),
-            filter(({ x, y, z }) => x !== null && y !== null && z !== null)
-          )
-          .subscribe(
-            (orientationData) => {
-              setOrientation(orientationData);
-            },
-            (error) => {
-              console.error(error);
-            }
-          );
-          
-                return () => {
-                 
-                  accelerometerSubscription.unsubscribe();
-                };
-              }, [orientation]);
-              useEffect(() => {
-                if (orientation.x === 0 && orientation.y > 0 && orientation.z === 0) {
-                  setRotation(0);
-                } else if (orientation.x > 0 && orientation.y === 0 && orientation.z === 0) {
-                  setRotation(270);
-                } else if (orientation.x === 0 && orientation.y < 0 && orientation.z === 0) {
-                  setRotation(180);
-                } else if (orientation.x < 0 && orientation.y === 0 && orientation.z === 0) {
-                  setRotation(90);
-                }
-              }, [orientation]);
-              useEffect(() => {
-                if(location !== null){
-                  if(location.coords.speed>X){
-                    setSelectedImage(images[1]);}
-                    if(location.coords.speed>Y && location.coords.speed<X){
-                      setSelectedImage(images[2]);}
-                      if(location.coords.speed<Y){
-                        setSelectedImage(images[3]);}
-                      }
-                
-              }, [location]);
-            
-    return(
-      <SafeAreaView style={styles.container}>
-          <ScrollView style={styles.scrollView}>
-          {  location && <View>
-        <Text>X: {X}</Text>
-        <Text>Y: {Y}</Text>
-        <Text>Speed: {location.coords.speed}</Text>
-        <Image source={selectedImage} style={styles.image} />
-        </View>
-        }
+const Screen1 = ({ navigation }) => (
+  <SafeAreaView style={styles.container}>
 
-        <Text>X: {orientation.x}</Text>
-        <Text>Y: {orientation.y}</Text>
-        <Text>Z: {orientation.z}</Text>
-        <Image source={require('../Resource/6.jpg')}     style={[styles.image, { transform: [{ rotate: `${rotation}deg` }] } ]} />
-      
-        </ScrollView>
-      </SafeAreaView>
-      );};
-      const Screen3 = ({ navigation }) => {
-      
-      return(
-        <SafeAreaView style={styles.container}>
-          <Text style={styles.text2}>IMAGE HAS GREEN COLOR ICON</Text>
-          <Text style={styles.text1}> VIDEO HAS RED COLOR ICON</Text>
-          <FlatList
-  ref={flatListRef1}
-  data={loadPhotosVideos}
-  renderItem={({ item }) => {
-    const isVideo = item.name.endsWith('.mp4');
-   
-    return (
-      <TouchableOpacity onPress={() => handlePress(item)}>
-        <ImageBackground source={{ uri: 'file://' + item.path }} style={styles.image}>
-          <View>
-         
-            <Icon name={'camera'} size={20} color={isVideo ? 'red' : 'green'} />
-            
-          </View>
-        </ImageBackground>
-       </TouchableOpacity>
+  <Button title="Switch camera" onPress={toggleCameraType} color="orange" />
+  <Button title="Switch mode" onPress={toggleCameraMode} color="red" />
+  <Button title="Take photo/ Record video" onPress={TakePhotoOrRecordVideo} color="green" />
+  {photo && <Image source={{ uri: photo }} style={{ width: 200, height: 200 }} />}
+</SafeAreaView>
+);
+const Screen2 = ({ navigation }) => {
+
+  useEffect(()  => {
+    const getLocation=()=>{
+    const locationPermission =  requestLocationPermission();
+    if (!locationPermission) {
+      console.log('location permission denied');
+      return;
+    }
+    Geolocation.getCurrentPosition(
+      (position) => {
+        setLocation(position);
+      },
+      (error) => {
+        console.error(error);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );};
+    getLocation();
+    const locationInterval = setInterval(getLocation, 10000);
+    return () => {
+      clearInterval(locationInterval);
+     
+    };
+  }, [location]);
+    useEffect(()  => {
+
+    setUpdateIntervalForType(SensorTypes.accelerometer, 500);
+    const accelerometerSubscription = accelerometer
+    .pipe(
+      map(({ x, y, z }) => ({ x, y, z })),
+      filter(({ x, y, z }) => x !== null && y !== null && z !== null)
+    )
+    .subscribe(
+      (orientationData) => {
+        setOrientation(orientationData);
+      },
+      (error) => {
+        console.error(error);
+      }
     );
-  }}
-  horizontal={true}
-  showsHorizontalScrollIndicator={true}
-  refreshControl={
-    <RefreshControl refreshing={refreshing} onRefresh={onRefresh1} />
+    
+          return () => {
+           
+            accelerometerSubscription.unsubscribe();
+          };
+        }, [orientation]);
+        useEffect(() => {
+          if (orientation.x === 0 && orientation.y > 0 && orientation.z === 0) {
+            setRotation(0);
+          } else if (orientation.x > 0 && orientation.y === 0 && orientation.z === 0) {
+            setRotation(270);
+          } else if (orientation.x === 0 && orientation.y < 0 && orientation.z === 0) {
+            setRotation(180);
+          } else if (orientation.x < 0 && orientation.y === 0 && orientation.z === 0) {
+            setRotation(90);
+          }
+        }, [orientation]);
+        useEffect(() => {
+          if(location !== null){
+            if(location.coords.speed>X){
+              setSelectedImage(images[1]);}
+              if(location.coords.speed>Y && location.coords.speed<X){
+                setSelectedImage(images[2]);}
+                if(location.coords.speed<Y){
+                  setSelectedImage(images[3]);}
+                }
+          
+        }, [location]);
+      
+return(
+<SafeAreaView style={styles.container}>
+    <ScrollView style={styles.scrollView}>
+    {  location && <View>
+  <Text>X: {X}</Text>
+  <Text>Y: {Y}</Text>
+  <Text>Speed: {location.coords.speed}</Text>
+  <Image source={selectedImage} style={styles.image} />
+  </View>
   }
+
+  <Text>X: {orientation.x}</Text>
+  <Text>Y: {orientation.y}</Text>
+  <Text>Z: {orientation.z}</Text>
+  <Image source={require('../Resource/6.jpg')}     style={[styles.image, { transform: [{ rotate: `${rotation}deg` }] } ]} />
+
+  </ScrollView>
+</SafeAreaView>
+);};
+const Screen3 = ({ navigation }) => {
+  useEffect(() => {
+    if(isnagivate)
+    navigation.navigate('Screen4');
+  }, []);
+return(
+  <SafeAreaView style={styles.container}>
+    <Text style={styles.text2}>IMAGE HAS GREEN COLOR ICON</Text>
+    <Text style={styles.text1}> VIDEO HAS RED COLOR ICON</Text>
+    <FlatList
+ref={flatListRef1}
+data={loadPhotosVideos}
+renderItem={({ item }) => {
+const isVideo = item.name.endsWith('.mp4');
+
+return (
+<TouchableOpacity onPress={() => handlePress(item)}>
+  <ImageBackground source={{ uri: 'file://' + item.path }} style={styles.image}>
+    <View>
+   
+      <Icon name={'camera'} size={20} color={isVideo ? 'red' : 'green'} />
+      
+    </View>
+  </ImageBackground>
+ </TouchableOpacity>
+);
+}}
+horizontal={true}
+showsHorizontalScrollIndicator={true}
+refreshControl={
+<RefreshControl refreshing={refreshing} onRefresh={onRefresh1} />
+}
 />
 {visible &&
 <TextInput
-          style={styles.input}
-          onChangeText={(newT)=>ChangeText(newT)}
-          onSubmitEditing={submitFunction}
-          autoFocus={true}
-         // value={childRef.current ? childRef.current?.ChangeText : ''}
-        />
-      }
-       
-    </SafeAreaView>
-      );};
+    style={styles.input}
+    onChangeText={(newT)=>ChangeText(newT)}
+    onSubmitEditing={submitFunction}
+    autoFocus={true}
+    value={newName}
+  />
+}
+ 
+</SafeAreaView>
+);};
+const Screen4 = ({ navigation }) =>{
+return (
+  <SafeAreaView style={styles.container}>
 
-
-
+{ispassItem ? (
+  <Image source={{ uri: 'file://' + passItem.path}} style={styles.image2} />
+) : (
+  <Text>No media selected</Text>
+)}
+</SafeAreaView>
+);};
     return(
         <NavigationContainer>
 <Tab.Navigator initialRouteName='Screen1' screenOptions={{unmountOnBlur:true}} >
       <Tab.Screen name="Screen1" component={Screen1}   options={{  title: 'Camera', headerTitleAlign: 'center',headerTintColor:"blue" }} />
       <Tab.Screen name="Screen2" component={Screen2}   options={{  title: 'sensors', headerTitleAlign: 'center',headerTintColor:"blue" }} />
       <Tab.Screen name="Screen3" component={Screen3}   options={{  title: 'gallery', headerTitleAlign: 'center',headerTintColor:"blue" }} />
-
+      <Tab.Screen name="Screen4" component={Screen4}   options={{  title: 'Media viewer', headerTitleAlign: 'center',headerTintColor:"blue" }} />
 </Tab.Navigator>
         </NavigationContainer>
     );
@@ -576,8 +621,16 @@ const styles = StyleSheet.create({
     image: {
       width: 40,
       height: 40,
-      marginBottom: 20,
+      marginBottom: 10,
+      marginTop:30,
     },
+    image2: {
+      width: 200,
+      height: 200,
+      marginBottom: 10,
+      marginTop:30,
+    },
+
 
     screen2style: {
       flex: 1,
@@ -589,20 +642,25 @@ const styles = StyleSheet.create({
     },
     text1: {
       fontSize: 10,
-      marginTop: 30,
+      marginTop: 10,
       color:"red",
+      fontWeight:'bold',
     },
     text2: {
       fontSize: 10,
-      marginTop: 30,
+      marginTop: 10,
       color:"green",
+      fontWeight:'bold',
+
     },
     input: {
       height: 40,
       margin: 12,
-      borderWidth: 1,
+      borderWidth: 5,
+      borderColor:'red',
       padding: 10,
       color:"purple",
+      fontWeight:"bold",
     },
 
   });
